24c20535875825659c63802ecec95ee7197db170
Zorin Mikhail <rivixxx@gmail.com>
2022-04-18 14:33:53 +0300

    add block learning (no response)

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import "../Css/Reset.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import SectionBox from "../SectionBest/SectionBest";
+import LearningFormat from "../LearningFormat/LearningFormat";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Header />
             <Main />
             <SectionBox well = "100+" material = "3 000" profession = "15" members = "2 000"/>
+            <LearningFormat />
         </div>
     );
 }
---
+++ src/LearningFormat/LearningFormat.js
@@ -0,0 +1,23 @@
+import MainBtn from "../ReplayComponents/MainBtn";
+import "./LearningFormat.css";
+
+function LearningFormat() {
+    return (
+        <section className="learning" style={{ backgroundColor: "#10B582", alignItems: "center" }}>
+            <div className="learning__container" style={{ display: "flex", padding: "150px 0 120px 0" }}>
+                <div className="learning__left" style={{ flex: "1 1 50%" }}>
+                    <div className="learning__image">
+                        <img src="./img/Learning/learning-photo.png" alt="Титульная картинка" title="Фото учебного процесса" />
+                    </div>
+                </div>
+                <div className="learning__right" style={{ display: "flex", flexDirection: "column", alignItems: "flex-start",  flex: "1 1 50%" }}>
+                    <h1 className="learning-title">Выбирайте свой формат обучения</h1>
+                    <p className="learning-text">Смотрите видео-лекции в удобное для вас время, обучайтесь на тренажере, участвуйте в интенсивных воркшопах с командой или проходите курс с гибким расписанием. На нашей платформе каждый найдет подходящий формат.</p>
+                    <MainBtn name="Все курсы" />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default LearningFormat;
