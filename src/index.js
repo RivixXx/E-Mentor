f30063fc5b2eb472ce6e41ed400749c95a979f0a
Zorin Mikhail <rivixxx@gmail.com>
2022-04-22 00:59:07 +0300

    test upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {HashRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import App from './App/App';
 import AppPage2 from "./App/AppPage2";
 
@@ -9,12 +9,12 @@ const root = ReactDOM.createRoot(document.getElementById('wrapper'));
 root.render (
   
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}/>
         <Route path="/AppPage2" element={<AppPage2 />}/>
-        <Route path="/App" element={<App />}/>
+        {/* <Route path="/App" element={<App />}/> */}
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
